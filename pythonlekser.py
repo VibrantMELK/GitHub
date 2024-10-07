430200159a5401c3479f5e169df870186699b9c2
“VibrantMELK” <gatchaemilie@gmail.com>
2024-10-07 14:37:50 +0200

    legget til et skrik

---
+++ pythonlekser.py
@@ -0,0 +1,58 @@
+#oppgave 1
+alder = 4
+print("alder: ", + alder)
+alder = 17
+print("den nye alderenen er: ", + alder)
+
+#oppgave 2
+a = 4
+b = 5
+print(a + b)
+
+c = int(input("skriv inn et tall: "))
+d = int(input("skriv inn et tall: "))
+print(c+d)
+
+#oppgave 3
+a = 3
+b = 5
+
+if a > b:
+    print("a er større enn b")
+else:
+    print("a er ikke større enn b")
+if a == b:
+    print("tallene er like verdi")
+
+#oppgave 4
+hovedstat = input("skriv en hovedstat i skandinavia: ")
+
+if hovedstat == "oslo":
+    print("riktig!")
+elif hovedstat == "stockholm":
+    print("riktig!")
+elif hovedstat == "københavn":
+    print("riktig!")
+else:
+    print("feil!")
+
+#oppgave 5
+alder = int(input("hvor gammel er du?: "))
+
+if alder < 9:
+    print("billetten er gratis!")
+elif alder < 17:
+    print("billetten koster: 20 kr")
+elif alder == 18 < 65:
+    print("billetten koster: 40 kr")
+elif alder > 65:
+    print("billetten koster: 20 kr")
+
+#oppgave 6
+poengsum = poeng * spørsmål
+poeng = 1
+spørsmål = input("emi_ie")
+
+if spørsmål == emilie:
+    print("riktig", + poeng)
+else
