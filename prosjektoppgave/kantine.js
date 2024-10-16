e540fff561da3fb1c51b860efdf6e5f9b27dfb2c
“VibrantMELK” <gatchaemilie@gmail.com>
2024-10-16 13:24:57 +0200

    endrer litt på koden

--- prosjektoppgave/kantine.js
+++ prosjektoppgave/kantine.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMcontentLoaded", function() {
-    const days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
-    const today = new Date().getDay();
-    const dagensMeny = document.querySelector(`.meny-dag:nth-child(${today})`);
-  
-    if (dagensMeny) {
-      dagensMeny.style.backgroundColor = "#d4edda";
-    }
+      const days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+  const today = new Date().getDay();
+  const dagensMeny = document.querySelector(`.meny-dag:nth-child(${today})`);
+
+  if (dagensMeny) {
+    dagensMeny.style.backgroundColor = "#ffffff";
+  }
 });
