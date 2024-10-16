23393ed1e3c8034beb64f1289ac75d3984d3f8a8
“VibrantMELK” <gatchaemilie@gmail.com>
2024-10-16 09:55:54 +0200

    ferdig med js kode

--- prosjektoppgave/kantine.js
+++ prosjektoppgave/kantine.js
@@ -1,1 +1,9 @@
-document.addEventListener()
+document.addEventListener("DOMcontentLoaded", function() {
+    const days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+    const today = new Date().getDay();
+    const dagensMeny = document.querySelector(`.meny-dag:nth-child(${today})`);
+  
+    if (dagensMeny) {
+      dagensMeny.style.backgroundColor = "#d4edda";
+    }
+});
