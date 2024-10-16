98533c9924dfe646ffa44fba2ace5ec8e26b2410
“VibrantMELK” <gatchaemilie@gmail.com>
2024-10-16 14:11:30 +0200

    fikset js kode

--- prosjektoppgave/kantine.js
+++ prosjektoppgave/kantine.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMcontentLoaded", function() {
-      const days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
-  const today = new Date().getDay();
-  const dagensMeny = document.querySelector(`.meny-dag:nth-child(${today})`);
-
-  if (dagensMeny) {
-    dagensMeny.style.backgroundColor = "#d4edda";
-  }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+    const today = new Date().getDay();
+    const dagensMeny = document.querySelector(`.meny-dag:nth-child(${today})`);
+  
+    if (dagensMeny) {
+      dagensMeny.style.backgroundColor = "rgb(255, 0, 0)";
+    }
+  });
+  
