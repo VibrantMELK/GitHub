59ea841a4da9ca58e5219fc8ee28a39275ac5e09
“VibrantMELK” <gatchaemilie@gmail.com>
2024-10-28 13:54:32 +0100

    endrer på js kode for å bli enklere

--- prosjektoppgave/kantine.js
+++ prosjektoppgave/kantine.js
@@ -1,6 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const days = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
-    const today = new Date().getDay();
+document.addEventListener("DOMContentLoaded", () => {
+    const dagensUkedag = new Date().getDay();
     const dagensMeny = document.querySelector(`.meny-dag:nth-child(${today})`);
   
     if (dagensMeny) {
